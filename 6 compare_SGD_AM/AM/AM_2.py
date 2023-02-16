12f38237f72503164716bed797e654104c4a700e
kathkkkkkk <106976287+kathkkkkkk@users.noreply.github.com>
2023-02-16 09:47:57 +0100

    Add files via upload

---
+++ 6 compare_SGD_AM/AM/AM_2.py
@@ -0,0 +1,70 @@
+from generate_data import generate_data
+from AM_algorithm_1 import algorithm_1 
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+x_train, y_train, x_validation, y_validation, x_test, y_test, x_test_sb, y_test_sb = generate_data()
+
+
+# compute the SB of ARFF 
+d = 1
+
+M = 2000
+K = 2**10
+lambda_ = 1
+delta = 0.5 # 0.05 list of parameters 
+gamma = 3*d - 2
+
+beta, w, f_est_train, pred_validation, error_train_list, error_validation_list, error_valid_min, error_test_list, error_test_end, FVU_validation, SB_M1, SB_M2 = algorithm_1(
+                                            x_train, y_train, 
+                                            x_test, y_test, 
+                                            x_validation, y_validation,
+                                            x_test_sb, y_test_sb,  
+                                            K, M, lambda_, delta, gamma)
+
+np.savez('./am_2.npz', 
+         x_train = x_train, 
+         y_train = y_train, 
+         x_validation = x_validation, 
+         y_validation = y_validation, 
+         beta = beta, w = w, f_est_train = f_est_train, 
+         pre_validation = pred_validation, 
+         error_train_list = error_train_list, 
+         error_validation_list = error_validation_list, 
+         error_valid_min = error_valid_min, 
+         error_test_list = error_test_list, 
+         error_test_end = error_test_end, 
+         FVU_validation = FVU_validation, 
+         SB_M1 = SB_M1, 
+         SB_M2 = SB_M2)
+
+plt.subplot(2, 2, 1)
+plt.plot(x_validation, pred_validation, '*', label = 'validation')
+plt.plot(x_test_sb, y_test_sb, label = 'func')
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+plt.title("Estimation")
+plt.legend()
+                      
+t_vec = np.arange(0, M)
+plt.subplot(2, 2, 2)
+plt.semilogy(t_vec[2:], error_train_list[2:], label='training_error')
+plt.semilogy(t_vec[2:], error_validation_list[2:], label='validation_error')
+plt.semilogy(t_vec[2:], error_test_list[2:], label='test_error')
+plt.title("Error")
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.plot(t_vec[1:], FVU_validation[1:])
+plt.title("FVU")
+
+plt.subplot(2, 2, 4)
+plt.plot(t_vec[1:], SB_M1[1:], color='blue', label="Method 1")
+plt.plot(t_vec[1:], SB_M2[2:], color='red', label="Method 2")
+plt.title("Spetral Bias")
+plt.legend()
+
+plt.tight_layout()
+plt.savefig('./am_2.jpg')
+plt.show()
---
+++ 6 compare_SGD_AM/SGD/neural_network.py
@@ -0,0 +1,76 @@
+from platform import node
+import numpy as np 
+import tensorflow as tf 
+from tensorflow import keras 
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Model
+
+
+# define the keras model 
+def nn_define_1(nodes = 64, 
+                kernel_initializer=tf.keras.initializers.he_normal, 
+                bias_initializer=tf.keras.initializers.Zeros()): 
+    input_layer = keras.Input(shape = (1, ))
+    hidden_layer_1 = keras.layers.Dense(units = nodes, 
+                                    activation = 'relu', 
+                                    kernel_initializer=tf.keras.initializers.he_normal, 
+                                    bias_initializer=tf.keras.initializers.Zeros())
+    hidden_layer_2 = keras.layers.Dense(units = nodes, 
+                                    activation = 'relu', 
+                                    kernel_initializer=tf.keras.initializers.he_normal, 
+                                    bias_initializer=tf.keras.initializers.Zeros())
+    hidden_layer_3 = keras.layers.Dense(units = nodes, 
+                                    activation = 'relu', 
+                                    kernel_initializer=tf.keras.initializers.he_normal, 
+                                    bias_initializer=tf.keras.initializers.Zeros())
+    hidden_layer_4 = keras.layers.Dense(units = nodes, 
+                                    activation = 'relu', 
+                                    kernel_initializer=tf.keras.initializers.he_normal, 
+                                    bias_initializer=tf.keras.initializers.Zeros())
+    hidden_layer_5 = keras.layers.Dense(units = nodes, 
+                                    activation = 'relu', 
+                                    kernel_initializer=tf.keras.initializers.he_normal, 
+                                    bias_initializer=tf.keras.initializers.Zeros())
+    output_layer = keras.layers.Dense(units = 1, 
+                                    use_bias = False)
+    model = keras.Sequential([input_layer, 
+                            hidden_layer_1,
+                            hidden_layer_2, 
+                            hidden_layer_3, 
+                            hidden_layer_4, 
+                            hidden_layer_5, 
+                            output_layer])
+    
+    return model 
+
+
+
+def nn_define_2(input_dim = 1, 
+                output_dim = 1, 
+                num_layers = 1, 
+                num_nodes = 2**8, 
+                activation=tf.keras.activations.relu, 
+                kernel_initializer=tf.keras.initializers.he_normal, 
+                bias_initializer=tf.keras.initializers.Zeros()): 
+    
+    tf.keras.backend.set_floatx('float64')
+    
+    # Define the network. This class corresponds to an MLP.
+    # input layer
+    inputs = tf.keras.Input(shape=(input_dim,), name="Input")
+
+    # Hidden Layers
+    x = Dense(units=num_nodes, activation=activation, kernel_initializer=kernel_initializer,
+                  bias_initializer=bias_initializer)(inputs)
+    for i in np.arange(1, num_layers):
+        x = Dense(units=num_nodes, activation=activation, kernel_initializer=kernel_initializer,
+                      bias_initializer=bias_initializer)(x)
+    # Outputs
+    outputs = Dense(output_dim, activation="linear", name="predictions")(x)
+    if output_dim == 3:
+        outputs = Dense(output_dim, activation="linear", name="predictions")(x)
+
+    # Compile the model
+    model = Model(inputs=inputs, outputs=outputs)
+    
+    return model
