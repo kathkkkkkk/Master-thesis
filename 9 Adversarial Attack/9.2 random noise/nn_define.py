8ed9bdac30506b176d9b75373a374534d9bbf1e8
kathkkkkkk <106976287+kathkkkkkk@users.noreply.github.com>
2023-06-13 12:32:32 +0200

    Add files via upload

---
+++ 9 Adversarial Attack/9.2 random noise/nn_define.py
@@ -0,0 +1,26 @@
+import tensorflow as tf 
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
+
+# define model 
+# Neural network 
+
+def NN_define(input_size = 784, m_size = 2**5, output_size = 1): 
+    # encoding 
+    input_img = tf.keras.Input(shape=(input_size,))
+
+
+    # middle layer 
+    activation = tf.math.cos 
+    m_layer = Dense(m_size, activation=activation, 
+                    kernel_initializer=tf.keras.initializers.he_normal, 
+                    bias_initializer=tf.keras.initializers.Zeros())(input_img)
+    #m_layer = Dense(m_size, activation='relu')(h2_layer)
+
+    output_img = Dense(output_size, activation='linear')(m_layer)
+
+    model = Model(input_img, output_img)
+
+    print(model.summary())
+    
+    return model 
